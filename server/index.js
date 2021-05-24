2625720b490fbec68f5ffe3701609b6d07dca865
unknown <bar.yaron2018@gmail.com>
2021-05-24 12:16:36 +0300

    adding component from material table component still in progress

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+app.use(cors());
+
+const categoreis = [{id: 1, name:"מעדנייה"}, {id: 2, name:"מוצרי חלב"}, {id: 3, name:"ירקות"}, {id: 4, name:"שימורים"}, {id: 5, name:"תינוקות"}];
 
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
+    res.json({ status: 200, categoreis: categoreis});
   });
 
 app.listen(PORT, () => {
